9453a452620249bc3d2664fff47203c7661ba170
Markus Schirp <mbj@seonic.net>
2013-02-01 14:16:50 +0100

    Use identifier as repository name dm-session => datamapper/dm-session

--- lib/status/repository.rb
+++ lib/status/repository.rb
@@ -2,7 +2,7 @@ module Status
 
   # A dm-2 related GIT repository (not a dm-2 stuff thing, sorry *g*)
   class Repository
-    include Adamantium::Flat, Composition.new(:identifier)
+    include Adamantium::Flat, Composition.new(:name)
 
     # Return github api url
     #
@@ -11,19 +11,9 @@ module Status
     # @api private
     #
     def github_api_url
-      "https://api.github.com/repo/#{identifier}"
+      "https://api.github.com/repo/#{name}"
     end
     memoize :github_api_url
-
-    # Return repository name
-    #
-    # @return [String]
-    #
-    # @api private
-    #
-    def name
-      identifier.split('/', 2).last
-    end
 
     # Return travis api rul
     #
@@ -32,7 +22,7 @@ module Status
     # @api private
     #
     def travis_api_url
-      "https://api.github.com/repo/#{identifier}"
+      "https://api.github.com/repo/#{name}"
     end
     memoize :travis_api_url
 
@@ -43,7 +33,7 @@ module Status
     # @api private
     #
     def travis_status_href
-      "https://travis-ci.org/#{identifier}"
+      "https://travis-ci.org/#{name}"
     end
     memoize :travis_status_href
 
@@ -54,7 +44,7 @@ module Status
     # @api private
     #
     def travis_image_src
-      "https://travis-ci.org/#{identifier}.png?branch=master"
+      "https://travis-ci.org/#{name}.png?branch=master"
     end
     memoize :travis_image_src
 
@@ -65,7 +55,7 @@ module Status
     # @api private
     #
     def gemnasium_status_href
-      "https://gemnasium.com/#{identifier}"
+      "https://gemnasium.com/#{name}"
     end
     memoize :gemnasium_status_href
 
@@ -76,7 +66,7 @@ module Status
     # @api private
     #
     def gemnasium_image_src
-      "https://gemnasium.com/#{identifier}.png"
+      "https://gemnasium.com/#{name}.png"
     end
     memoize :gemnasium_image_src
 
