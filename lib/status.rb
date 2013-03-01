7af55c307d9ab583f71bb729bc70238968825860
Markus Schirp <mbj@seonic.net>
2013-03-01 22:49:58 +0100

    Update root namespace
    It now keeps track of all stuff that does NOT depend on configuration.

--- lib/status.rb
+++ lib/status.rb
@@ -7,16 +7,65 @@ require 'compass'
 
 Sass.load_paths.concat(Compass::Configuration::Data.new('foo').sass_load_paths)
 
+# Namespace for dm-status
 module Status
 
   # Return application
   #
-  # @return [Stoelting::Application]
+  # @return [Status::Application]
   #
   # @api private
   #
   def self.application
-    @application ||= Application.new(Pathname.new(__FILE__).parent.parent, 'status')
+    @application || raise("No application set")
+  end
+
+  # Set application
+  #
+  # @param [Status::Application] application
+  #
+  # @return [self]
+  #
+  # @api private
+  #
+  def self.application=(application)
+    if defined?(@application)
+      raise('application is already defined')
+    end
+    @application = application
+    self
+  end
+
+  # Return template path
+  #
+  # @return [Pathname]
+  #
+  # @api private
+  #
+  def self.template_path
+    @template_path ||= root.join('templates').freeze
+  end
+
+  # Return template for name
+  #
+  # @param [String] name
+  #
+  # @return [Tilt::Template]
+  # 
+  # @api private
+  #
+  def self.template(name)
+    Tilt.new(template_path.join(name).to_s)
+  end
+
+  # Return root directory
+  #
+  # @return [Pathname]
+  #
+  # @api private
+  #
+  def self.root
+    @root ||= Pathname.new(__FILE__).parent.parent
   end
 
   # Fetch resource from cache or locally
