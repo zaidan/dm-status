547f1d4779bd2085b3598f12c76083b7e7b1e5a9
Markus Schirp <mbj@seonic.net>
2013-03-03 14:45:47 +0100

    Remove the rest of the cache infrastructure

--- lib/status.rb
+++ lib/status.rb
@@ -42,38 +42,8 @@ module Status
     @root ||= Pathname.new(__FILE__).parent.parent
   end
 
-  # Fetch resource from cache or locally
-  #
-  # @param [String] url
-  #   the url to fetch
-  #
-  # @param [Class:Presenter] presenter
-  #   the presenter to instantiate on new data
-  #
-  # @return [Presenter]
-  #   presenter instance
-  #   
-  # @api private
-  #
-  def self.fetch(url, presenter)
-    cache.fetch(url) do 
-      Request.run(url, presenter)
-    end
-  end
-
-  # Return cache
-  #
-  # @return [Cache]
-  #
-  # @api private
-  #
-  def self.cache
-    @cache ||= Cache.new
-  end
-
 end
 
-require 'status/cache'
 require 'status/application'
 require 'status/repository'
 require 'status/action'
