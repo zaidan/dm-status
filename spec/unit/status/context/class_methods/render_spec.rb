69e923a6ff15343c4ac18d882c70b9e5cc43ecca
Markus Schirp <mbj@seonic.net>
2013-03-03 18:23:29 +0100

    Kill mutations in Context.render

---
+++ spec/unit/status/context/class_methods/render_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Status::Context, '.render' do
+  let(:object) { described_class::Page }
+
+  subject { object.render(attributes) }
+
+  let(:attributes) { 
+    { 
+      :title            => 'TITLE', 
+      :meta_description => 'META', 
+      :content          => 'CONTENT'
+    }
+  }
+
+  # Yeah these are weak expectations
+  its(:content) { should include('TITLE') }
+  its(:content) { should include('META') }
+  its(:content) { should include('CONTENT') }
+end
