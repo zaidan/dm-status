cf0a809eea17aa521123d1f6e04b9f168e56c4fd
Markus Schirp <mbj@seonic.net>
2013-03-03 17:29:28 +0100

    Add a smoky integration spec

---
+++ spec/integeration/web/smoke_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature 'smoky smoky' do
+  scenario 'visiting root' do
+    visit('/')
+    page.status_code.should be(200)
+  end
+
+  scenario 'visiting not found' do
+    visit('/not-found')
+    page.status_code.should be(404)
+  end
+
+  scenario 'visiting css' do
+    visit('/assets/application.css')
+    page.status_code.should be(200)
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,10 @@
 require 'devtools'
 require 'status'
 Devtools.init_spec_helper
+
+require 'capybara'
+require 'capybara/rspec'
+application = Status::Application.build_from_config_path(
+  Pathname.new(__FILE__).parent.join('config.yml')
+)
+Capybara.app = Joy::Adapter::Rack.new(application)
