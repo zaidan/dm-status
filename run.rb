d81a4744e602233453d8632f8bfeb20b6b6dad43
Firas Zaidan <firas@zaidan.de>
2013-05-31 23:50:37 +0200

    Remove run.rb, use joy-runner status instead!

--- run.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-$: << 'lib'
-require 'status'
-application = Status::Application.build_from_config_path(Pathname(__FILE__).parent.join('config.yml'))
-Joy::Runner.run(application)
