5a32097ce455f522b55ba3a67ee4ced5348830ac
Firas Zaidan <firas@zaidan.de>
2013-06-05 15:10:00 +0200

    Add action for tags

---
+++ spec/unit/status/action/tag/response_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Status::Action::Tag, '#response' do
+  let(:object) { described_class.new(application, request) }
+
+  subject { object.response }
+
+  let(:application) { Spec.application                           }
+  let(:tag)         { 'tag1'                                     }
+  let(:get)         { { 'tag' => tag }                           }
+  let(:request)     { mock('Request', :query_params_hash => get) }
+
+  its(:status) { should be(Response::Status::OK) }
+  its(:body) do
+    projects = Spec.application.projects.select { |project| project.has_tag?(tag) }
+    content = Status::Context::Main.render(:projects => projects)
+    should eql(Status::Context::Page.render(Spec.application.page_attributes.merge(:content => content)).to_s)
+  end
+end
